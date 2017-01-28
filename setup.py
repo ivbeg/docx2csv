d09bd0c3e976deadf3a9bf49c793670606a10e95
Phil Krylov <phil.krylov@gmail.com>
2017-01-28 17:23:53 +0300

    Sepearate CLI script from Python module to allow direct usage from Python

--- setup.py
+++ setup.py
@@ -2,13 +2,18 @@
 
 from distutils.core import setup
 
+
 setup(name='docx2csv',
       version='1.0',
       description='Table extractor from .docx files',
       author='Ivan Begtin',
       author_email='ibegtin@gmail.com',
       url='https://github.com/ivbeg/docx2csv',
-#      packages=['distutils', 'distutils.command'],
-    scripts=['docx2csv'],
-     )
-
+      packages=['docx2csv'],
+      scripts=['bin/docx2csv'],
+      install_requires=[
+          'click',
+          'python-docx',
+          'xlwt',
+      ],
+      )
